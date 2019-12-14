74a390daf78deda84fc873fafd64a8483d2d597e
Tim Cockerill <dtimcockerill2@gmail.com>
2019-12-14 12:29:21 -0500

    student directory view

---
+++ dojobase/src/pages/StudentDir.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function StudentDir() {
+    return (
+      <div>
+        <h1 className="text-center">Student Directory</h1>
+      </div>
+    );
+  };
+
+export default StudentDir;
