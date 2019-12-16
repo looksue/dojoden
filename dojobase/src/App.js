9d34b2a5f577ce4882ee9c48b80bfc9139cb35b7
Tim Cockerill <dtimcockerill2@gmail.com>
2019-12-16 18:52:02 -0500

    rendering with react, frame for everything

--- dojobase/src/App.js
+++ dojobase/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import StudentDir from "./pages/StudentDir";
 import Sidenav from "./components/sideNav"
+import Navbar from "./components/navbar"
 
 function App() {
   return (
     <Router>
       <div>
-        <Sidenav />  
+        <Sidenav />
+        <Navbar />  
      <Route exact path="/allStudents" component={StudentDir} />    
       </div>
      
--- dojobase/src/components/sideNav.js
+++ dojobase/src/components/sideNav.js
@@ -5,36 +5,33 @@ import "./style.css";
 function SideNav() {
     return (
         <div id="sidebar-wrapper">
-            <div class="row">
-                <div class="columns medium-12">
-                </div>
+            <div className="row">
+                <img src="assets/img/MataLeauLOGO_noBG.png" />
             </div>
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#">
-                        Dojo Manager </a>
-                </li>
-                <li>
-                    <a href="#">Students</a>
-                </li>
-                <li>
-                    <a href="#">Classes</a>
-                </li>
-                <li>
-                    <a href="#">Messaging</a>
-                </li>
-                <li>
-                    <a href="#">Calendar</a>
-                </li>
-                <li>
-                    <a href="#">Company</a>
-                </li>
-                <li class="active">
-                    <a href="#">Billing</a>
-                </li>
-            </ul>
+            <div className="row menu selections">
+                <ul className="sidebar-nav">
+                    <li>
+                        <a href="#">Students</a>
+                    </li>
+                    <li>
+                        <a href="#">Classes</a>
+                    </li>
+                    <li>
+                        <a href="#">Messaging</a>
+                    </li>
+                    <li>
+                        <a href="#">Calendar</a>
+                    </li>
+                    <li>
+                        <a href="#">Company</a>
+                    </li>
+                    <li>
+                        <a href="#">Billing</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
-}
+};
 
 export default SideNav;
