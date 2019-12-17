ad5aecc0ea84276b3dbe3940f3eb9b5be0402c16
Daniel Goldman <dan@Ozmos-MacBook-Pro.local>
2019-12-16 20:59:07 -0500

    added react components X5

---
+++ dojobase/src/pages/ManagerAttendance.js
@@ -0,0 +1,406 @@
+{
+  /* manager attendance */
+}
+import React, { Component } from "react";
+
+function ManagerAttendance() {
+  return (
+    <div class="container-fluid">
+      <div className="row">
+        <div className="large-12">
+          <h1>Attendance</h1>
+          <p className="lead">Attendance Per Class.</p>
+          <ul className="tabs" data-tabs id="panel-content293">
+            <li className="tabs-title is-active">
+              <a href="#panel294" aria-selected="true" id="liFundamentals">
+                Fundamentals
+              </a>
+            </li>
+            <li className="tabs-title">
+              <a href="#panel295" id="liAdvanced">
+                Advanced
+              </a>
+            </li>
+            <li className="tabs-title">
+              <a href="#panel296" id="liCompTeam">
+                Comp Team
+              </a>
+            </li>
+          </ul>
+          <div className="tabs-content" data-tabs-content="panel-content293">
+            <div className="tabs-panel is-active" id="panel294">
+              <table>
+                <thead className="theadAttendance">
+                  <tr>
+                    <th width="80%">Student Name</th>
+                    <th width="20%">Mark Present</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Brian Helms</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Sue Look</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Daniel Goldman</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <a href="#" className="button btnAttendance" id="btnAttendance">
+                Submit Attendance
+              </a>
+            </div>
+            <div className="tabs-panel" id="panel295">
+              <table>
+                <thead>
+                  <tr>
+                    <th width="80%">Student Name</th>
+                    <th width="20%">Mark Present</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Brian Helms</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Sue Look</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Daniel Goldman</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Brian Helms</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Sue Look</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Daniel Goldman</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <a href="#" className="button">
+                Submit Attendence
+              </a>
+            </div>
+            <div className="tabs-panel" id="panel296">
+              <table>
+                <thead>
+                  <tr>
+                    <th width="80%">Student Name</th>
+                    <th width="20%">Mark Present</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Brian Helms</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Sue Look</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Daniel Goldman</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Brian Helms</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Sue Look</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Daniel Goldman</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Brian Helms</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Sue Look</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Daniel Goldman</td>
+                    <td>
+                      <div className="switch">
+                        <input
+                          className="switch-input"
+                          id="activeStudent"
+                          type="checkbox"
+                          name="activeStudent"
+                        />
+                        <label
+                          className="switch-paddle"
+                          htmlFor="activeStudent"
+                        />
+                        <span className="show-for-sr">Active Student</span>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <a href="#" className="button">
+                Submit Attendence
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ManagerAttendance;
