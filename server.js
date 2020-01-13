0242c5b398d8e4663080f7a582b4b428e6bdd6e1
Sue Look <suelook1@gmail.com>
2020-01-13 15:21:25 -0500

    delete local schema, re-test with Heroku

--- server.js
+++ server.js
@@ -58,8 +58,8 @@ app.get("*", (req, res) => {
   });  
 
   db.sequelize.sync().then(function() {
-    //app.listen(PORT, function() {
-    //  console.log("App listening on PORT " + PORT);
-    //});
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
   });
   
