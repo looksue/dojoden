9cf7b4ff7e85fcf81644d271a689e988a291801c
Sue Look <suelook1@gmail.com>
2020-01-13 12:44:22 -0500

    added function to allow crossdomain

--- server.js
+++ server.js
@@ -4,8 +4,31 @@ const PORT = process.env.PORT || 3000; // was 3001
 const app = express();
 var db = require("./dojobase/models");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+//app.configure(function () {
+  // app.use(express.bodyParser());
+  // app.use(express.methodOverride());
+  // app.use(app.router);
+   app.use(allowCrossDomain);
+  // app.use(express.static(path.join(application_root, "public")));
+  // app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+ //});
+
+ app.use(express.urlencoded({ extended: true }));
+ app.use(express.json());
 
 // app.use(express.static("public"));
 if (process.env.NODE_ENV === "production") {
